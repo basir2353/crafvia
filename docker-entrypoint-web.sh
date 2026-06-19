0bc42f26bb9d0d5b476f5a6074ad252cc26cdf31
Abdul Basit Dogar <mabdulbasitdogar@gmail.com>
2026-06-19 12:27:23 +0500

    mj

--- docker-entrypoint-web.sh
+++ docker-entrypoint-web.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 set -e
 
-export PORT="${PORT:-80}"
-export API_UPSTREAM="${API_UPSTREAM:-http://api:3001}"
+PORT="${PORT:-80}"
+API_UPSTREAM="${API_UPSTREAM:-http://api:3001}"
 
-envsubst '${PORT} ${API_UPSTREAM}' \
-  < /etc/nginx/templates/default.conf.template \
+sed \
+  -e "s|__PORT__|${PORT}|g" \
+  -e "s|__API_UPSTREAM__|${API_UPSTREAM}|g" \
+  /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
+echo "Starting nginx on port ${PORT}, API upstream ${API_UPSTREAM}"
+nginx -t
 exec nginx -g 'daemon off;'
